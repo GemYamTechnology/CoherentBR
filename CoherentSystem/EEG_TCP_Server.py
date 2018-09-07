51d57159921cfc35194d304bf4661a2668d075b5
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2018-09-07 21:34:47 +0800

    TCP.py
    將TCP.py簡化製作成模組，應用在EEG_TCP_Server.py，之後會利用TCP模組改良System.py

--- CoherentSystem/EEG_TCP_Server.py
+++ CoherentSystem/EEG_TCP_Server.py
@@ -5,71 +5,11 @@
 A simple "tcp echo server" for demonstrating TCP usage.
 The server listens for TCP packets and echoes any received
 packets back to the originating host.
-
+20180907使用TCP模組來進行程式的簡化，會將內容轉移到System.py
 """
-import MySQLdb
-import socket
-import optparse
-import time
-import datetime
-import sys
-import DataBase
-
-def EchoClient(ServerHost, ServerPort):
-    print "TCP Echo Data"
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-    try:
-        s.bind((ServerHost, ServerPort))#開始監聽的IP與Port
-        s.listen(1)#設定連線上限1個
-    except socket.error, msg:
-        print "ERROR: ", msg#顯示錯誤，
-        s.close()
-        s = None
-
-    if s is None:#如果s = None
-        sys.exit(1)#離開程式
-
-    #while 1:
-    print "Listen of Host and Port: %s:%d"%(ServerHost, ServerPort)#Listen Host of ServerHost and ServerPort
-    data_len = 0
-    try:
-        conn, addr = s.accept()#將sockt「網路介面」的資訊放入conn，遠端連線資訊存入addr
-    except KeyboardInterrupt:
-        print "Closing Connection"
-        s.close()
-        s = None
-        sys.exit(1)
-    AllList = [conn, addr]
-    return AllList
-
-def EchoData(conn, addr):
-    try:
-        data = conn.recv(4096)#將EEG送來的資料存入date變數裡面
-        if not data: break
-
-        date =  time.strftime("%Y/%m/%d %H:%M:%S")
-        d = datetime.datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
-        date1 = time.mktime(d.timetuple()) + 1e-6 * d.microsecond
-
-        ClientIP, ClientPort = addr
-        print "Incoming connection accepted: " + str(ClientIP) + ":" + str(ClientPort)#Client of IP and Port
-
-        print date, addr[0], ":", repr(data)#顯示時間、IP、Port、TCP資料
-        print ""
-        conn.send(data)
-    except KeyboardInterrupt:
-        print "Closing Connection"
-        s.close()
-        s = None
-        sys.exit(1)
-
-    conn.close()
-    ReprData = repr(data)
-    AllList = [date1, ClientIP, ServerHost, ReprData]
-    return AllList
+import TCP
 
 ServerHost, ServerPort = "",50007
 while(1):
-     EchoData = EchoClient(ServerHost, ServerPort)
-     EchoData(EchoData)
+     conn, addr, ServerHost, ServerPort = TCP.EchoClient(ServerHost, ServerPort)
+     TCP.EchoData(conn, addr, ServerHost, ServerPort)
--- CoherentSystem/TCP.py
+++ CoherentSystem/TCP.py
@@ -16,64 +16,65 @@ def EEG_TCP_Client(target_host, target_port, string):
 	client.send(string)
 
 	response = client.recv(4096)
-	time.sleep(1)
-	print response
-	print ""
+	#print response
+	#print ""
 	return response
 
 def EchoClient(ServerHost, ServerPort):
 	if ServerHost and ServerPort == "":
 		ServerHost, ServerPort = "", 50007
-	print "TCP Echo Data"
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-	try:
-		s.bind((ServerHost, ServerPort))#開始監聽的IP與Port
-		s.listen(1)#設定連線上限1個
-    except socket.error, msg:
-		print "ERROR: ", msg#顯示錯誤，
-		s.close()
-		s = None
+	else:
+		print "TCP Echo Data"
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+		try:
+			s.bind((ServerHost, ServerPort))#開始監聽的IP與Port
+			s.listen(1)#設定連線上限1個
+		except socket.error, msg:
+			print "ERROR: ", msg#顯示錯誤，
+			s.close()
+			s = None
 
-	if s is None:#如果s = None
-		sys.exit(1)#離開程式
+		if s is None:#如果s = None
+			sys.exit(1)#離開程式
 
-	print "Listen of Host and Port: %s:%d"%(ServerHost, ServerPort)#Listen Host of ServerHost and ServerPort
-	data_len = 0
-	try:
-		conn, addr = s.accept()#將sockt「網路介面」的資訊放入conn，遠端連線資訊存入addr
-	except KeyboardInterrupt:
-		print "Closing Connection"
-		s.close()
-		s = None
-		sys.exit(1)
-	AllList = [conn, addr]
-	return AllList
+		print "Listen of Host and Port: %s:%d"%(ServerHost, ServerPort)#Listen Host of ServerHost and ServerPort
+		data_len = 0
+		try:
+			conn, addr = s.accept()#將sockt「網路介面」的資訊放入conn，遠端連線資訊存入addr
+		except KeyboardInterrupt:
+			print "Closing Connection"
+			s.close()
+			s = None
+			sys.exit(1)
+		AllList = [conn, addr, ServerHost, ServerPort]
+		return AllList
 
-def EchoData(conn, addr):
+def EchoData(conn, addr, ServerHost, ServerPort):
 	if conn and addr == "":
-		EchoClient(ServerHost, ServerPort)
-    try:
-		data = conn.recv(4096)#將EEG送來的資料存入date變數裡面
-		if not data: break
+		conn, addr = EchoClient()
+	else:
+		try:
+			data = conn.recv(4096)#將EEG送來的資料存入date變數裡面
+			#if not data:break
 
-		date =  time.strftime("%Y/%m/%d %H:%M:%S")
-		d = datetime.datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
-		date1 = time.mktime(d.timetuple()) + 1e-6 * d.microsecond
+			date =  time.strftime("%Y/%m/%d %H:%M:%S")
+			d = datetime.datetime.strptime(date, '%Y/%m/%d %H:%M:%S')
+			date1 = time.mktime(d.timetuple()) + 1e-6 * d.microsecond
 
-		ClientIP, ClientPort = addr
-		print "Incoming connection accepted: " + str(ClientIP) + ":" + str(ClientPort)#Client of IP and Port
+			ClientIP, ClientPort = addr
+			print "Incoming connection accepted: " + str(ClientIP) + ":" + str(ClientPort)#Client of IP and Port
 
-		print date, addr[0], ":", repr(data)#顯示時間、IP、Port、TCP資料
-		print ""
-		conn.send(data)
-	except KeyboardInterrupt:
-		print "Closing Connection"
-		s.close()
-		s = None
-		sys.exit(1)
+			print date, addr[0], ":", repr(data)#顯示時間、IP、Port、TCP資料
+			print ""
+			conn.send(data)
+		except KeyboardInterrupt:
+			print "Closing Connection"
+			s.close()
+			s = None
+			sys.exit(1)
 
-	conn.close()
-	ReprData = repr(data)
-	AllList = [date1, ClientIP, ServerHost, ReprData]
-	return AllList
+		conn.close()
+		ReprData = repr(data)
+		AllList = [date1, ClientIP, ClientPort, ServerHost, ServerPort, ReprData]
+		return AllList
