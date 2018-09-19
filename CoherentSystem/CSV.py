24d6975acd2f4a8c0cde7160ed98e9e4b819ce35
Wu Chenzhi <38396747+timmy61109@users.noreply.github.com>
2018-09-19 14:05:07 +0800

    編輯CSV.py，讓存檔的程式模組化，雖然感覺可以不用模組化。

--- CoherentSystem/CSV.py
+++ CoherentSystem/CSV.py
@@ -3,8 +3,26 @@
 # coding = UTF-8
 import csv
 
-f8 = open(a + 'recv_text8.csv', 'w')
-f.write( repr(data) + "\n" )
-f1.close()#創建檔案結束
+def WriteCSV(FileName, Data):
+    file = open(FileName, 'w')
+    file.write(Data)
+    file.close()#創建檔案結束
 
-open('249recv_text1.csv','r').read()
+def CreateCSV(FileName):
+    file = open(FileName, 'w')
+    file.close()#創建檔案結束
+
+def ReadNoDealWithCSV(FileName):
+    with open(FileName, 'rb') as csvfile:#讀取資料
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    return spamreader
+
+def ReadDealWithCSV(FileName):
+    graph_data1 = open(FileName, 'r').read()
+    lines1 = graph_data1.split('\n')
+    return lines1
+
+def aa(FileName, Data):#打開一個文件用於讀寫。如果該文件已存在，文件指針將會放在文件的結尾。文件打開時會是追加模式。如果該文件不存在，創建新文件用於讀寫。
+    file = open(FileName, 'at+')
+    file.write(Data)
+    file.close()
