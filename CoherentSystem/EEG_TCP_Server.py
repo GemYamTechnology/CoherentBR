d1d50fd1a9abcaa99bbadb12a2f3a3e281dfc2c2
Wu Chenzhi <38396747+timmy61109@users.noreply.github.com>
2018-09-20 15:34:03 +0800

    使用檔案除紋

--- CoherentSystem/EEG_TCP_Server.py
+++ CoherentSystem/EEG_TCP_Server.py
@@ -8,8 +8,16 @@ packets back to the originating host.
 20180907使用TCP模組來進行程式的簡化，會將內容轉移到System.py
 """
 import TCP
-
-ServerHost, ServerPort = "",50007
+import FileAccess as FA
+i = 0
+FileName = "./Data.csv"
+ServsderHost, ServerPort = "",50007
+CreateCSV(FileName)
 while(1):
+    i += 1
     conn, addr, ServerHost, ServerPort = TCP.EchoClient(ServerHost, ServerPort)
     date1, ClientIP, ClientPort, ServerHost, ServerPort, ReprData = TCP.EchoData(conn, addr, ServerHost, ServerPort)
+    Data = date1 + " " + ClientIP + " " + ClientPort + " " + ServerHost + " " + ServerPort + " " + ReprData + '\n'
+    FA.WriteCSV(FileName, Data)
+    if i == 81:
+        CreateCSV(FileName)
